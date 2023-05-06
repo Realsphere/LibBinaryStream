4f33599efb11980e57c758dc52c64759b18cdd51
David Gerson <84614495+pogrammerX@users.noreply.github.com>
2023-05-06 14:18:04 +0200

    Update copyright notice

--- LibBinaryStream.h
+++ LibBinaryStream.h
@@ -1,5 +1,5 @@
 /*
-LibBinaryStream, Copyright (c) 2023 Realsphere Entertainment
+LibBinaryStream, Copyright (c) 2015 - 2023 Realsphere Entertainment
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, subject to the following conditions:
